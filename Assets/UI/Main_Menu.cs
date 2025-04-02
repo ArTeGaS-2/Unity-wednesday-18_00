07f316a37ca898692dcd7910ba479bb8ce92e8c7
ArTeGaS-2 <probablytorvik@gmail.com>
2025-04-02 19:59:29 +0300

    Самостійна з додавання кнопок меню

--- Assets/UI/Main_Menu.cs
+++ Assets/UI/Main_Menu.cs
@@ -5,9 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+    public GameObject startButton;
+    public GameObject levelButtons;
+    private void Start()
+    {
+        startButton.SetActive(true);
+        levelButtons.SetActive(false);
+    }
     public void Start_Game()
     {
-        //Scene inGame = SceneManager.GetSceneAt(1);
+        startButton.SetActive(false);
+        levelButtons.SetActive(true);
+    }
+    public void LoadLevel_1()
+    {
         SceneManager.LoadScene(1);
     }
+    public void LoadLevel_2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadLevel_3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
 }
