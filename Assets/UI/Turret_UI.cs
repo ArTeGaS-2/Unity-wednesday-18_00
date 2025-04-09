f60cc7f6ddad775352e2195c9997678c07e45583
ArTeGaS-2 <probablytorvik@gmail.com>
2025-04-09 19:59:22 +0300

    Інтерфейс турелі на BuildMenu та Turret_UI

---
+++ Assets/UI/Turret_UI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Turret_UI : MonoBehaviour
+{
+    public static Turret_UI instance;
+    public GameObject upgrade_buy_UI;
+
+    [Header("Текстові елементи")]
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI DPS_Text;
+    public TextMeshProUGUI radiusText;
+    public TextMeshProUGUI DMG_Type_Text;
+    public TextMeshProUGUI priceText;
+}
