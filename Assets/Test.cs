4f3fd26ebe025ecef3d1b46b63b3da0709c9baa8
ArTeGaS-2 <probablytorvik@gmail.com>
2025-04-23 20:05:32 +0300

    Тести.

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    ///
+    /// 1. У BuildMenuUPD.cs за вибір активного слота відповідає змінна:
+    ///     a) currentObject
+    ///     b) currentFrameUI
+    ///     c) highlightColor
+    ///     d) frame1
+    /// 2. Компоненти frame1, frame2, frame3 мають тип:
+    ///     a) Image
+    ///     b) RawImage
+    ///     c) Button
+    ///     d) SpriteRenderer
+    /// 3. Щоб приховати панель турелі, скрипт викликає:
+    ///     a) turretPanel.Hide
+    ///     b) 
+    ///     c) 
+    ///     d) 
+    ///
+
+}
